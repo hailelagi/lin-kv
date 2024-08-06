9e5564bd858cc347f0e8b470493314d7d665b73b
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2024-08-06 17:15:35 +0100

    wip
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

---
+++ maelstrom-datomic/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+
+	n.Handle("echo", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["type"] = "echo_ok"
+
+		return n.Reply(msg, body)
+	})
+
+	// Execute the node's message loop. This will run until STDIN is closed.
+	if err := n.Run(); err != nil {
+		log.Printf("ERROR: %s", err)
+		os.Exit(1)
+	}
+}
